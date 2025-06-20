94ffa1caacab203fb13fbb1f9ae86a818d69421e
MGS-sails <mahfouzshehu@gmail.com>
2025-06-20 16:38:08 +0100

    Fixed data format bug on genome detail endpoint

--- emgapiv2/api/genomes.py
+++ emgapiv2/api/genomes.py
@@ -20,7 +20,27 @@ def get_genome(request, accession: str):
         Genome.objects.select_related("biome", "geo_origin", "catalogue").prefetch_related("pangenome_geographic_range"),
         accession=accession
     )
-    return genome
+    return {
+        "accession": genome.accession,
+        "ena_genome_accession": genome.ena_genome_accession,
+        "ena_sample_accession": genome.ena_sample_accession,
+        "ncbi_genome_accession": genome.ncbi_genome_accession,
+        "img_genome_accession": genome.img_genome_accession,
+        "patric_genome_accession": genome.patric_genome_accession,
+        "biome_id": genome.biome.id,
+        "length": genome.length,
+        "num_contigs": genome.num_contigs,
+        "n_50": genome.n_50,
+        "gc_content": genome.gc_content,
+        "type": genome.type,
+        "completeness": genome.completeness,
+        "contamination": genome.contamination,
+        "catalogue_id": genome.catalogue.id,
+        "geographic_origin": genome.geographic_origin,
+        "geographic_range": genome.geographic_range,
+        "last_update": genome.last_update.isoformat(),
+        "first_created": genome.first_created.isoformat(),
+    }
 
 
 @router.get("/{accession}/annotations", response=GenomeWithAnnotations, summary="Get genome annotations by accession", operation_id="get_genome_annotations")
