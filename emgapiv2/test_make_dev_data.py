d76c604533d8cf1f6ef0682f781013b78c3e8d75
Sandy Rogers <sandyr@ebi.ac.uk>
2025-07-11 17:48:36 +0100

    adds real ASAV6 fixtures and data-maker to dev data

--- emgapiv2/test_make_dev_data.py
+++ emgapiv2/test_make_dev_data.py
@@ -4,7 +4,8 @@ import pytest
 from django.core.management import call_command
 
 from analyses.models import Biome, Run, Analysis
-from workflows.flows.analyse_study_tasks.import_completed_amplicon_analyses import (
+from workflows.flows.analyse_study_tasks.import_completed_assembly_analyses import (
+    import_completed_assembly_analysis,
     import_completed_analysis as import_completed_amplicon_analysis,
 )
 from workflows.flows.analyse_study_tasks.import_completed_rawreads_analyses import (
@@ -69,6 +70,31 @@ def amplicon_analysis_with_downloads(
         },
     }
     analysis.save()
+    import_completed_amplicon_analysis(analysis)
+
+
+@pytest.fixture
+@patch("workflows.flows.analyse_study_tasks.copy_v6_pipeline_results.move_data")
+def assembly_analysis_with_downloads(mock_copy_flow, mgnify_assemblies_completed):
+    assem = mgnify_assemblies_completed[0]
+    assem.add_erz_accession(
+        "ERZ857107"
+    )  # n.b. does not correspond to this run in real ena
+
+    study = assem.reads_study
+    sample = assem.sample
+
+    analysis = Analysis.objects.create(
+        ena_study=study.ena_study,
+        study=study,
+        experiment_type=Run.ExperimentTypes.ASSEMBLY,
+        sample=sample,
+        assembly=assem,
+    )
+    analysis.mark_status(analysis.AnalysisStates.ANALYSIS_COMPLETED)
+    analysis.results_dir = "/app/data/tests/assembly_v6_output/ERZ857107/ERZ857107"
+    analysis.save()
+    import_completed_assembly_analysis(analysis)
     import_completed_amplicon_analysis(analysis)
 
 
@@ -141,6 +167,7 @@ def test_make_dev_data(
     study_downloads,
     mgnify_assemblies_completed,
     amplicon_analysis_with_downloads,
+    assembly_analysis_with_downloads,
     prefect_harness,
     geographic_locations,
     genome_catalogues,
@@ -149,7 +176,7 @@ def test_make_dev_data(
     private_study_with_download,
 ):
     """
-    Dummy test that just sets up fixtures and dumps them to JSON for using as dev data.
+    Stub test that just sets up fixtures and dumps them to JSON for using as dev data.
     """
 
     assert Biome.objects.count() == 4
