6f84db802b81718f7341e6bfbb192453607515a1
MGS-sails <mahfouzshehu@gmail.com>
2025-07-15 19:20:30 +0100

    Added example to genomebase

--- genomes/schemas/GenomeBase.py
+++ genomes/schemas/GenomeBase.py
@@ -1,11 +1,13 @@
 from ninja import Schema
 from typing import Optional
 
+from pydantic import Field
+
 from analyses.schemas import Biome
 
 
 class GenomeBase(Schema):
-    accession: str
+    accession: str = Field(..., examples=["MGYG000000001"])
     ena_genome_accession: Optional[str]
     ena_sample_accession: Optional[str]
     ncbi_genome_accession: Optional[str]
