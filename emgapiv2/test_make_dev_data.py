3772b54296e0d231562f9b0d4689c2ce2b938bcc
Sandy Rogers <sandyr@ebi.ac.uk>
2025-07-18 10:25:44 +0100

    fixed consolidated analysis dev fixtures

--- emgapiv2/test_make_dev_data.py
+++ emgapiv2/test_make_dev_data.py
@@ -4,12 +4,11 @@ import pytest
 from django.core.management import call_command
 
 from analyses.models import Biome, Run, Analysis
-from workflows.flows.analyse_study_tasks.import_completed_assembly_analyses import (
-    import_completed_assembly_analysis,
+from workflows.flows.analyse_study_tasks.import_completed_amplicon_analyses import (
     import_completed_analysis as import_completed_amplicon_analysis,
 )
-from workflows.flows.analyse_study_tasks.import_completed_rawreads_analyses import (
-    import_completed_analysis as import_completed_rawreads_analysis,
+from workflows.flows.analyse_study_tasks.import_completed_assembly_analyses import (
+    import_completed_assembly_analysis,
 )
 
 
@@ -95,67 +94,67 @@ def assembly_analysis_with_downloads(mock_copy_flow, mgnify_assemblies_completed
     analysis.results_dir = "/app/data/tests/assembly_v6_output/ERZ857107/ERZ857107"
     analysis.save()
     import_completed_assembly_analysis(analysis)
-    import_completed_amplicon_analysis(analysis)
 
 
-@pytest.fixture
-@patch("workflows.flows.analyse_study_tasks.copy_v6_pipeline_results.move_data")
-def rawreads_analysis_with_downloads(
-    mock_copy_flow, raw_reads_mgnify_study, raw_reads_mgnify_sample
-):
-    sample = raw_reads_mgnify_sample[0]
-    study = raw_reads_mgnify_study
-
-    run = Run.objects.create(
-        ena_accessions=["SRR1111111"],
-        study=study,
-        ena_study=study.ena_study,
-        sample=sample,
-        experiment_type=Run.ExperimentTypes.METAGENOMIC,
-        metadata={
-            Run.CommonMetadataKeys.FASTQ_FTPS: ["ftp://example.org/SRR1111111.fastq"]
-        },
-    )
-
-    analysis = Analysis.objects.create(
-        ena_study=study.ena_study,
-        study=study,
-        experiment_type=Run.ExperimentTypes.METAGENOMIC,
-        sample=sample,
-        run=run,
-    )
-    analysis.mark_status(analysis.AnalysisStates.ANALYSIS_COMPLETED)
-
-    analysis.results_dir = "/app/data/tests/rawreads_v6_output/SRR1111111"
-    analysis.metadata[analysis.KnownMetadataKeys.MARKER_GENE_SUMMARY] = {
-        analysis.TAXONOMIC: {
-            "marker_genes": {
-                "MOTUS": {"Eukarya": {"read_count": 0, "majority_marker": False}},
-                "LSU": {
-                    "Archaea": {"read_count": 0, "majority_marker": False},
-                    "Eukarya": {"read_count": 0, "majority_marker": False},
-                    "Bacteria": {"read_count": 0, "majority_marker": False},
-                },
-                "SSU": {
-                    "Archaea": {"read_count": 65, "majority_marker": True},
-                    "Eukarya": {"read_count": 0, "majority_marker": True},
-                    "Bacteria": {"read_count": 28655, "majority_marker": True},
-                },
-            }
-        },
-        analysis.FUNCTIONAL_ANNOTATION: {
-            "amplified_regions": [
-                {
-                    "asv_count": 94,
-                    "read_count": 16664,
-                    "marker_gene": "16S",
-                    "amplified_region": "V3-V4",
-                }
-            ]
-        },
-    }
-    analysis.save()
-    import_completed_rawreads_analysis(analysis)
+# TODO: currently unused as download data fixtures are missing
+# @pytest.fixture
+# @patch("workflows.flows.analyse_study_tasks.copy_v6_pipeline_results.move_data")
+# def rawreads_analysis_with_downloads(
+#     mock_copy_flow, raw_reads_mgnify_study, raw_reads_mgnify_sample
+# ):
+#     sample = raw_reads_mgnify_sample[0]
+#     study = raw_reads_mgnify_study
+#
+#     run = Run.objects.create(
+#         ena_accessions=["SRR1111111"],
+#         study=study,
+#         ena_study=study.ena_study,
+#         sample=sample,
+#         experiment_type=Run.ExperimentTypes.METAGENOMIC,
+#         metadata={
+#             Run.CommonMetadataKeys.FASTQ_FTPS: ["ftp://example.org/SRR1111111.fastq"]
+#         },
+#     )
+#
+#     analysis = Analysis.objects.create(
+#         ena_study=study.ena_study,
+#         study=study,
+#         experiment_type=Run.ExperimentTypes.METAGENOMIC,
+#         sample=sample,
+#         run=run,
+#     )
+#     analysis.mark_status(analysis.AnalysisStates.ANALYSIS_COMPLETED)
+#
+#     analysis.results_dir = "/app/data/tests/rawreads_v6_output/SRR1111111"
+#     analysis.metadata[analysis.KnownMetadataKeys.MARKER_GENE_SUMMARY] = {
+#         analysis.TAXONOMIES: {
+#             "marker_genes": {
+#                 "MOTUS": {"Eukarya": {"read_count": 0, "majority_marker": False}},
+#                 "LSU": {
+#                     "Archaea": {"read_count": 0, "majority_marker": False},
+#                     "Eukarya": {"read_count": 0, "majority_marker": False},
+#                     "Bacteria": {"read_count": 0, "majority_marker": False},
+#                 },
+#                 "SSU": {
+#                     "Archaea": {"read_count": 65, "majority_marker": True},
+#                     "Eukarya": {"read_count": 0, "majority_marker": True},
+#                     "Bacteria": {"read_count": 28655, "majority_marker": True},
+#                 },
+#             }
+#         },
+#         analysis.FUNCTIONAL_ANNOTATION: {
+#             "amplified_regions": [
+#                 {
+#                     "asv_count": 94,
+#                     "read_count": 16664,
+#                     "marker_gene": "16S",
+#                     "amplified_region": "V3-V4",
+#                 }
+#             ]
+#         },
+#     }
+#     analysis.save()
+#     import_completed_rawreads_analysis(analysis)
 
 
 @pytest.mark.dev_data_maker
@@ -163,11 +162,11 @@ def rawreads_analysis_with_downloads(
 def test_make_dev_data(
     top_level_biomes,
     assemblers,
-    raw_read_analyses,
     study_downloads,
     mgnify_assemblies_completed,
     amplicon_analysis_with_downloads,
     assembly_analysis_with_downloads,
+    raw_read_analyses,
     prefect_harness,
     geographic_locations,
     genome_catalogues,
