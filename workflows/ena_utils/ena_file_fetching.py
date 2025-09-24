c23d55a8078098021d1274508719037c912ee4c9
Germana Baldi <Ge94@users.noreply.github.com>
2025-09-24 11:52:03 +0100

    Linting

--- workflows/ena_utils/ena_file_fetching.py
+++ workflows/ena_utils/ena_file_fetching.py
@@ -12,7 +12,9 @@ def convert_ena_ftp_to_fire_fastq(
 ) -> str:
     fire_url = ftp_url
     protocol_and_parts = fire_url.split("://")
-    fire_url = "ftp://" + protocol_and_parts[-1]  # replacing (if present) any protocol with ftp:// or adding it
+    fire_url = (
+        "ftp://" + protocol_and_parts[-1]
+    )  # replacing (if present) any protocol with ftp:// or adding it
 
     if not fire_url.startswith(EMG_CONFIG.ena.ftp_prefix):
         if raise_if_not_convertible:
