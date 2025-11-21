5c602692d59ca9239b12be71b9a64870a560d08a
Martin Beracochea <mbc@ebi.ac.uk>
2025-11-21 14:40:19 +0000

    Missing patches on test_make_dev_data
    # Conflicts:
    #	emgapiv2/test_make_dev_data.py

--- emgapiv2/test_make_dev_data.py
+++ emgapiv2/test_make_dev_data.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 import pytest
 from django.core.management import call_command
@@ -10,14 +10,17 @@ from workflows.flows.analyse_study_tasks.amplicon.import_completed_amplicon_anal
 
 
 @pytest.fixture
-@patch("workflows.flows.analyse_study_tasks.shared.copy_v6_pipeline_results.move_data")
+@patch(
+    "workflows.flows.analyse_study_tasks.shared.copy_v6_pipeline_results.run_deployment"
+)
 def amplicon_analysis_with_downloads(
-    mock_copy_flow,
+    mock_run_deployment,
     raw_reads_mgnify_study,
     raw_reads_mgnify_sample,
 ):
     sample = raw_reads_mgnify_sample[0]
     study = raw_reads_mgnify_study
+    mock_run_deployment.return_value = Mock(id="mock-flow-run-id")
 
     run = Run.objects.create(
         ena_accessions=["SRR1111111"],
@@ -69,6 +72,32 @@ def amplicon_analysis_with_downloads(
     }
     analysis.save()
     import_completed_amplicon_analysis(analysis)
+
+
+@pytest.fixture
+@patch(
+    "workflows.flows.analyse_study_tasks.shared.copy_v6_pipeline_results.run_deployment"
+)
+def assembly_analysis_with_downloads(mock_run_deployment, mgnify_assemblies_completed):
+    assem = mgnify_assemblies_completed[0]
+    assem.add_erz_accession(
+        "ERZ857107"
+    )  # n.b. does not correspond to this run in real ena
+
+    study = assem.reads_study
+    sample = assem.sample
+    mock_run_deployment.return_value = Mock(id="mock-flow-run-id")
+
+    analysis = Analysis.objects.create(
+        ena_study=study.ena_study,
+        study=study,
+        experiment_type=Run.ExperimentTypes.ASSEMBLY,
+        sample=sample,
+        assembly=assem,
+    )
+    analysis.mark_status(analysis.AnalysisStates.ANALYSIS_COMPLETED)
+    analysis.results_dir = "/app/data/tests/assembly_v6_output/ERP106708/ERZ857107"
+    analysis.save()
 
 
 # TODO: currently unused as download data fixtures are missing
