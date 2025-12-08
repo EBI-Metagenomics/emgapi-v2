1d9331e1b185e2cc0c85bcffd1eb04cf94e9095e
MGS-sails <mahfouzshehu@gmail.com>
2025-12-08 17:44:24 +0000

    Removed unecessary logger

--- workflows/flows/import_genomes_flow.py
+++ workflows/flows/import_genomes_flow.py
@@ -62,14 +62,9 @@ def get_catalogue(options):
     catalogue_dirname = os.path.basename(
         os.path.dirname(os.path.normpath(options["results_directory"]))
     )
-    # TODO: Update when transfer service URL root is finalised
-    # results_path_to_save = f"{EMG_CONFIG.service_urls.transfer_services_url_root}/genomes/{catalogue_dirname}/{options['catalogue_version']}"
     results_path_to_save = (
         f"/genomes/{catalogue_dirname}/{options['catalogue_version']}"
     )
-    logger = get_run_logger()
-    logger.info(f"Catalogue results path to save: {results_path_to_save}")
-
     catalogue_id = f"{options['catalogue_name'].replace(' ', '-')}-v{options['catalogue_version'].replace('.', '-')}".lower()
 
     catalogue, _ = GenomeCatalogue.objects.get_or_create(
