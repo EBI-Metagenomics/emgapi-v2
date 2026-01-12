1663b1777bca9caac8d60c080d009651f605c6da
MGS-sails <mahfouzshehu@gmail.com>
2026-01-12 20:23:06 +0000

    Created dedicated MGnifyGenomeDownloadFile class

--- genomes/schemas/GenomeDetail.py
+++ genomes/schemas/GenomeDetail.py
@@ -2,13 +2,10 @@ from typing import Optional, TYPE_CHECKING
 from genomes.schemas.GenomeBase import GenomeBase
 from genomes.schemas.GenomeCatalogue import GenomeCatalogueBase
 from ninja import Field
-
-if TYPE_CHECKING:
-    from analyses.schemas import MGnifyAnalysisDownloadFile
-
+from analyses.schemas import MGnifyGenomeDownloadFile
 
 class GenomeDetail(GenomeBase):
-    downloads: list["MGnifyAnalysisDownloadFile"] = Field(
+    downloads: list["MGnifyGenomeDownloadFile"] = Field(
         ..., alias="downloads_as_objects"
     )
     catalogue: Optional[GenomeCatalogueBase] = None
