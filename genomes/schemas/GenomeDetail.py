813ac9bf621e3a05f346b137b08d0240e77d4c2c
MGS-sails <mahfouzshehu@gmail.com>
2026-01-13 10:17:14 +0000

    Ran pre commit

--- genomes/schemas/GenomeDetail.py
+++ genomes/schemas/GenomeDetail.py
@@ -2,6 +2,7 @@ from typing import Optional, TYPE_CHECKING
 from genomes.schemas.GenomeBase import GenomeBase
 from genomes.schemas.GenomeCatalogue import GenomeCatalogueBase
 from ninja import Field
+
 if TYPE_CHECKING:
     from analyses.schemas import MGnifyGenomeDownloadFile
 
