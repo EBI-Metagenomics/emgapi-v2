5c782f7d80782b462453d3ee819132aac47ec519
MGS-sails <mahfouzshehu@gmail.com>
2026-01-13 09:20:42 +0000

    Ran precommit

--- genomes/schemas/GenomeDetail.py
+++ genomes/schemas/GenomeDetail.py
@@ -1,8 +1,9 @@
-from typing import Optional, TYPE_CHECKING
+from typing import Optional
 from genomes.schemas.GenomeBase import GenomeBase
 from genomes.schemas.GenomeCatalogue import GenomeCatalogueBase
 from ninja import Field
 from analyses.schemas import MGnifyGenomeDownloadFile
+
 
 class GenomeDetail(GenomeBase):
     downloads: list["MGnifyGenomeDownloadFile"] = Field(
