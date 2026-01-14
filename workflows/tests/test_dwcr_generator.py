7418cea27f3f3a6ee2b4d584478cfca9f4349fd0
Christian Atallah <chrisata@ebi.ac.uk>
2026-01-14 10:45:28 +0000

    mock requests.get calls instead of mocking the return value of get_all_ena_metadata()

--- workflows/tests/test_dwcr_generator.py
+++ workflows/tests/test_dwcr_generator.py
@@ -1,8 +1,6 @@
 import pytest
+import responses
 import pathlib
-from unittest.mock import patch
-from collections import defaultdict
-import pandas as pd
 
 
 from activate_django_first import EMG_CONFIG
@@ -15,6 +13,7 @@ from analyses.models import Study
 
 
 @pytest.mark.django_db(transaction=True)
+@responses.activate
 def test_dwcr_generator(
     amplicon_analysis_with_downloads,
     raw_reads_mgnify_study: Study,
@@ -25,30 +24,118 @@ def test_dwcr_generator(
         amplicon_analysis_with_downloads.pipeline_outdir
     )
 
-    mock_dict = defaultdict()
-    mock_dict["SRR1111111"] = pd.DataFrame(
-        {
-            "RunID": "SRR1111111",
-            "StudyID": "SRP2222222",
-            "SampleID": "SAMN3333333",
-            "seq_meth": "Illumina MiSeq",
-            "decimalLatitude": 12,
-            "decimalLongitude": 22,
-            "depth": 0.12,
-            "center_name": "Devonshire Building",
-            "temperature": 12,
-            "salinity": 0.12,
-            "country": "United Kingdom",
-            "collectionDate": "2025-05-25",
-        },
-        index=[0],
+    # Mock the read_run ENA API requests
+    responses.add(
+        responses.GET,
+        EMG_CONFIG.ena.portal_search_api,
+        json=[
+            {
+                "run_accession": "SRR1111111",
+                "secondary_study_accession": "SRP2222222",
+                "sample_accession": "SAMN3333333",
+                "instrument_model": "Illumina MiSeq",
+            }
+        ],
+        match=[
+            responses.matchers.query_param_matcher(
+                {
+                    "result": "read_run",
+                    "includeAccessions": "SRR1111111",
+                    "fields": "secondary_study_accession,sample_accession,instrument_model",
+                    "limit": "10",
+                    "format": "json",
+                    "download": "false",
+                }
+            )
+        ],
     )
-    with patch(
-        "mgnify_pipelines_toolkit.analysis.shared.dwc_summary_generator.get_all_ena_metadata_from_runs",
-        return_value=(mock_dict),
-    ):
-        generate_dwc_ready_summary_for_pipeline_run(
-            raw_reads_mgnify_study.accession,
-            amplicon_pipeline_outdir,
-            completed_runs_filename,
-        )
+
+    responses.add(
+        responses.GET,
+        EMG_CONFIG.ena.portal_search_api,
+        json=[
+            {
+                "run_accession": "SRR6180434",
+                "secondary_study_accession": "SRP4444444",
+                "sample_accession": "SAMN5555555",
+                "instrument_model": "Illumina MiSeq",
+            }
+        ],
+        match=[
+            responses.matchers.query_param_matcher(
+                {
+                    "result": "read_run",
+                    "includeAccessions": "SRR6180434",
+                    "fields": "secondary_study_accession,sample_accession,instrument_model",
+                    "limit": "10",
+                    "format": "json",
+                    "download": "false",
+                }
+            )
+        ],
+    )
+
+    # mock the sample ENA API requests
+    responses.add(
+        responses.GET,
+        EMG_CONFIG.ena.portal_search_api,
+        json=[
+            {
+                "lat": 12,
+                "lon": 22,
+                "collection_date": "2025-05-25",
+                "depth": 0.12,
+                "center_name": "Devonshire Building",
+                "temperature": 12,
+                "salinity": 0.12,
+                "country": "United Kingdom",
+            }
+        ],
+        match=[
+            responses.matchers.query_param_matcher(
+                {
+                    "result": "sample",
+                    "includeAccessions": "SAMN3333333",
+                    "fields": "lat,lon,collection_date,depth,center_name,temperature,salinity,country",
+                    "limit": "10",
+                    "format": "json",
+                    "download": "false",
+                }
+            )
+        ],
+    )
+
+    responses.add(
+        responses.GET,
+        EMG_CONFIG.ena.portal_search_api,
+        json=[
+            {
+                "lat": 12,
+                "lon": 22,
+                "collection_date": "2025-05-25",
+                "depth": 0.12,
+                "center_name": "Devonshire Building",
+                "temperature": 12,
+                "salinity": 0.12,
+                "country": "United Kingdom",
+            }
+        ],
+        match=[
+            responses.matchers.query_param_matcher(
+                {
+                    "result": "sample",
+                    "includeAccessions": "SAMN5555555",
+                    "fields": "lat,lon,collection_date,depth,center_name,temperature,salinity,country",
+                    "limit": "10",
+                    "format": "json",
+                    "download": "false",
+                }
+            )
+        ],
+    )
+
+    generate_dwc_ready_summary_for_pipeline_run(
+        raw_reads_mgnify_study.accession,
+        amplicon_pipeline_outdir,
+        completed_runs_filename,
+    )
