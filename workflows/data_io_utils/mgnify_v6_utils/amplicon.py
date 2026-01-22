e05c0e26eadab20731ebe03dfe66176c048d56ab
Germana Baldi <Ge94@users.noreply.github.com>
2026-01-22 12:51:02 +0000

    Prevent asv import if DADA2 error file exists

--- workflows/data_io_utils/mgnify_v6_utils/amplicon.py
+++ workflows/data_io_utils/mgnify_v6_utils/amplicon.py
@@ -316,6 +316,14 @@ def import_asv(analysis: analyses.models.Analysis, dir_for_analysis: Path):
         print(f"No asv dir in {dir_for_analysis}. Nothing to import.")
         return
 
+    dada2_stats_error = File(
+        path=dir_for_analysis / "qc" / f"{analysis.run.first_accession}_dada2_errors.txt"
+    )
+
+    if dada2_stats_error.exists():
+        print(f"ASV folder is empty, as dada2 wasn't able to run successfully. Nothing to import")
+        return
+
     asv_dir = Directory(
         path=dir_for_analysis  # /hps/prod/...../abc123/SRR999/
         / EMG_CONFIG.amplicon_pipeline.asv_folder,  # asv/
