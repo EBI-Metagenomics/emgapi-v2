d1b2b3489b05fbd7f2e9041daa92a85a3ab2158f
Sofia Ochkalova <so.ochkalova@gmail.com>
2026-01-26 09:47:05 +0000

    feat: add generate_assembly_analysis_pipeline_summary function (should be refactored later)

--- workflows/flows/analysis/assembly/tasks/assembly_analysis_pipeline_batch_study_summary_generator.py
+++ workflows/flows/analysis/assembly/tasks/assembly_analysis_pipeline_batch_study_summary_generator.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Union, List
 
 import click
+from ena.models import Study
 from prefect import get_run_logger, task
 
 from mgnify_pipelines_toolkit.analysis.assembly import study_summary_generator
@@ -79,6 +80,68 @@ def generate_assembly_analysis_pipeline_batch_summary(
     generated_files = list(
         study_dir.path.glob(f"{assembly_batch.id}*_{STUDY_SUMMARY_TSV}")
     )
+
+    logger.info(f"Assembly batch summary generator made files: {generated_files}")
+
+    return generated_files
+
+
+@task
+def generate_assembly_analysis_pipeline_summary(
+    study: Study,
+) -> Union[List[Path], None]:
+    """
+    Generate a study summary file for assembly analysis results (no batch context).
+
+    It will use the analyses where the workflow_status for ASA is set to COMPLETED.
+
+    The study summaries are written to the study.results_dir, these are partial files for a batch.
+    They will be merged later, and the assumption is that they live in this directory.
+
+    [NOTE]
+    There is a bit of repetition with workflows/flows/analyse_study_tasks/shared/study_summary.py
+    I (mbc) didn't want to refactor the former to account for batches, as we are testing the batch approach here.
+    If it works we can refactor the shared code to be more generic.
+
+    :param study: The Study object to summarize annotation results for
+    :return: List of paths to the study summary files generated
+    """
+
+    logger = get_run_logger()
+
+    logger.info(f"Generating assembly batch summary for {study.id}")
+
+    # ASA workspace contains the analysis results
+    asa_workspace = study.results_dir / "asa"
+    logger.info(f"Expecting to find analysis results in {asa_workspace}")
+
+    # Ensure the study has a results_dir to write summaries to
+    study.set_results_dir_default()
+
+    study_dir = Directory(
+        path=Path(study.results_dir),
+        rules=[DirectoryExistsRule],
+    )
+
+    logger.info(
+        f"Study results_dir, where summaries will be made, is {study.results_dir}"
+    )
+
+    with chdir(study.results_dir):
+        # TODO: we need to expose the summary as a lib component we can just import instead of having to use
+        #       click to bootstrap the environment
+        with click.Context(study_summary_generator.summarise_analyses) as ctx:
+            ctx.invoke(
+                study_summary_generator.summarise_analyses,
+                output_prefix=study.id,
+                assemblies=Path(
+                    study.results_dir / "asa" / "analysed_assemblies.csv"
+                ).absolute(),
+                study_dir=Path(asa_workspace).absolute(),
+                outdir=Path(study.results_dir).absolute(),
+            )
+
+    generated_files = list(study_dir.path.glob(f"{study.id}*_{STUDY_SUMMARY_TSV}"))
 
     logger.info(f"Assembly batch summary generator made files: {generated_files}")
 
