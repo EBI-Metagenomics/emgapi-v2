2d787432042ac8a5b737957675953db0528573ec
angelphanth <angel.phanth@hotmail.com>
2026-03-19 15:42:09 +0000

    adding experiment_type filter

--- emgapiv2/api/runs.py
+++ emgapiv2/api/runs.py
@@ -2,6 +2,7 @@ from ninja_extra import api_controller, http_get
 from ninja_extra.pagination import paginate
 from ninja_extra.schemas import NinjaPaginationResponseSchema
 from ninja_extra.exceptions import NotFound
+from ninja import FilterSchema
 
 import analyses.models
 from analyses.schemas import AnalysedRun, AnalysedRunDetail
@@ -9,6 +10,22 @@ from emgapiv2.api import ApiSections
 from emgapiv2.api import perms
 from emgapiv2.api.auth import WebinJWTAuth, DjangoSuperUserAuth, NoAuth
 from emgapiv2.api.perms import UnauthorisedIsUnfoundController
+from typing import Optional
+from pydantic import Field
+from django.db.models import Q
+from ninja import Query
+
+
+class RunListFilters(FilterSchema):
+    has_experiment_type: Optional[analyses.models.Run.ExperimentTypes] = Field(
+        None,
+        description="If set, will only show runs with the specified experiment type",
+    )
+
+    def filter_has_experiment_type(self, experiment_type: str | None) -> Q:
+        if not experiment_type:
+            return Q()
+        return Q(experiment_type=experiment_type)
 
 
 @api_controller("runs", tags=[ApiSections.RUNS])
@@ -29,10 +46,10 @@ class AnalysedRunController(UnauthorisedIsUnfoundController):
     @paginate()
     def list_analysed_runs(
         self,
-        # filters: BiomeListFilters = Query(...),
+        filters: RunListFilters = Query(...),
     ):
         qs = analyses.models.Run.public_objects.select_related()
-        # qs = filters.filter(qs)
+        qs = filters.filter(qs)
         return qs
 
     @http_get(
