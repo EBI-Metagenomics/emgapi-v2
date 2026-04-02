c68fb3cf63ae93375206070303dc4b5869746135
MGS-sails <mahfouzshehu@gmail.com>
2026-04-02 10:43:18 +0100

    Moved Sentry dsn to emg_config; Conditonally showing sentry-debug route based on debug mode enablement

--- emgapiv2/settings.py
+++ emgapiv2/settings.py
@@ -44,11 +44,6 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "insecure-dev-key")
 
 DEBUG = os.getenv("DJANGO_DEBUG", False)
 
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    send_default_pii=True,
-)
-
 
 def show_toolbar(request):
     return False
