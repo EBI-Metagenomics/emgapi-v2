ad5d1b4df812707aa632b1df5e7be6a1ee616806
Martin Beracochea <mbc@ebi.ac.uk>
2026-04-13 17:16:06 +0100

    Add a flow to sync the metadata for the studies

---
+++ workflows/tests/test_sync_studies_with_ena.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch
+
+import pytest
+
+from ena.models import Study
+from workflows.flows.housekeeping.sync_studies_with_ena import (
+    sync_studies_with_ena,
+)
+
+
+@patch(
+    "workflows.flows.housekeeping.sync_studies_with_ena.sync_study_metadata_from_ena"
+)
+@pytest.mark.django_db
+def test_sync_studies_with_ena_by_accessions(mock_sync, prefect_harness):
+    """Test that the flow syncs specific studies by accession, handling failures."""
+    prj_01, _ = Study.objects.get_or_create(accession="PRJNA000001", title="Study OK")
+    prj_02, _ = Study.objects.get_or_create(accession="PRJNA000002", title="Study Fail")
+
+    def mock_sync_study(study: Study):
+        if study.accession == "PRJNA000002":
+            raise RuntimeError("ENA error")
+        study.title = "Study OK - and updated!"
+        study.metadata["description"] = "Study OK - description updated!"
+        study.save()
+        return
+
+    mock_sync.side_effect = mock_sync_study
+
+    sync_studies_with_ena(
+        accessions=["PRJNA000001", "PRJNA000002"],
+        batch_size=10,
+    )
+
+    prj_01.refresh_from_db()
+    assert prj_01.title == "Study OK - and updated!"
+    assert prj_01.metadata["description"] == "Study OK - description updated!"
+
+    assert mock_sync.call_count == 2
