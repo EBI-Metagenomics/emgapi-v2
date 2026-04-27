42b97e81b2c62cda2f8d3104a711e658fb5c3aee
Sandy Rogers <sandyr@ebi.ac.uk>
2026-04-27 16:12:17 +0100

    Legacy data importer bugfixes (#335)
    * legacy data importer: add study download importing; fix new file paths; add exception handling
    
    * adds optional mgys_after parameter to legacy data importer flow deployments

--- workflows/flows/legacy/flows/import_legacy_analyses.py
+++ workflows/flows/legacy/flows/import_legacy_analyses.py
@@ -1,10 +1,28 @@
+import re
 from pathlib import Path
 
+from django.utils.text import slugify
 from prefect import get_run_logger
+from prefect.artifacts import create_table_artifact
 from sqlalchemy import select
 
 import activate_django_first  # noqa
-
+from analyses.base_models.with_downloads_models import (
+    DownloadFile,
+    DownloadFileType,
+    DownloadType,
+)
+from analyses.models import Analysis
+from workflows.data_io_utils.filenames import accession_prefix_separated_dir_path
+from workflows.data_io_utils.legacy_emg_dbs import (
+    LEGACY_DOWNLOAD_TYPE_MAP,
+    LEGACY_FILE_FORMATS_MAP,
+    LegacyStudy,
+    get_taxonomy_from_api_v1_mongo,
+    legacy_emg_db_session,
+    get_functions_from_api_v1_mongo,
+    LEGACY_PIPELINE_ID_MAP,
+)
 from workflows.ena_utils.ena_api_requests import (
     sync_privacy_state_of_ena_study_and_derived_objects,
     sync_study_metadata_from_ena,
@@ -22,23 +40,6 @@ from workflows.flows.legacy.tasks.make_sample_from_legacy_emg_db import (
 from workflows.flows.legacy.tasks.make_study_from_legacy_emg_db import (
     make_study_from_legacy_emg_db,
 )
-
-from analyses.base_models.with_downloads_models import (
-    DownloadFile,
-    DownloadFileType,
-    DownloadType,
-)
-from analyses.models import Analysis
-from workflows.data_io_utils.legacy_emg_dbs import (
-    LEGACY_DOWNLOAD_TYPE_MAP,
-    LEGACY_FILE_FORMATS_MAP,
-    LegacyStudy,
-    get_taxonomy_from_api_v1_mongo,
-    legacy_emg_db_session,
-    get_functions_from_api_v1_mongo,
-    LEGACY_PIPELINE_ID_MAP,
-)
-
 from workflows.prefect_utils.flows_utils import django_db_flow as flow
 
 
@@ -91,12 +92,23 @@ def import_legacy_analyses(mgys: str, fetch_functions: bool = False):
                     f"Analysis {legacy_analysis.job_id} has no linked run or assembly to import"
                 )
 
+            # External results dir for analysis:
+            # Drop the year and month prefix (first two parts), and add the 6-char study prefix.
+            # legacy_analysis.result_directory example: 2019/06/ERP108930/version_4.1/ERZ651/008/ERZ651768_FASTA
+            # study_prefix: ERP108
+            # expected: ERP108/ERP108930/version_4.1/ERZ651/008/ERZ651768_FASTA
+            res_dir_parts = Path(legacy_analysis.result_directory).parts
+            res_dir_stripped = Path(*res_dir_parts[3:])
+            study_prefix = accession_prefix_separated_dir_path(study.first_accession, 6)
+            analysis_external_results_dir = Path(study_prefix) / res_dir_stripped
+
             analysis, created = Analysis.objects.update_or_create(
                 id=legacy_analysis.job_id,
                 defaults={
                     "study": study,
                     "sample": sample,
                     "results_dir": legacy_analysis.result_directory,
+                    "external_results_dir": str(analysis_external_results_dir),
                     "ena_study": study.ena_study,
                     "pipeline_version": LEGACY_PIPELINE_ID_MAP.get(
                         legacy_analysis.pipeline_id
@@ -111,6 +123,10 @@ def import_legacy_analyses(mgys: str, fetch_functions: bool = False):
                 logger.info(f"Created analysis {analysis}")
             else:
                 logger.warning(f"Updated analysis {analysis}")
+
+            # clear existing downloads in case we are retrying
+            analysis.downloads = []
+            analysis.save()
 
             for legacy_download in legacy_analysis.downloads:
                 basename = Path(legacy_download.real_name)
@@ -155,12 +171,61 @@ def import_legacy_analyses(mgys: str, fetch_functions: bool = False):
         sync_privacy_state_of_ena_study_and_derived_objects(study.ena_study)
         sync_study_metadata_from_ena(study.ena_study)
 
+        # clear existing downloads in case we are retrying
+        study.downloads = []
+        study.save()
+        for legacy_study_download in legacy_study.study_downloads:
+            basename = Path(legacy_study_download.real_name)
+
+            if legacy_study_download.subdir:
+                path = Path(legacy_study_download.subdir.subdir) / basename
+            else:
+                path = basename
+
+            # Construct download group: study_summary.v{version}.{group_type_slug}
+            # The subdir contains the version, e.g. "version_5.0/project-summary"
+            version = 0
+            subdir_str = legacy_study_download.subdir.subdir
+            version_match = re.search(r"version_([\d\.]+)", subdir_str)
+            if version_match:
+                version = version_match.group(1)
+            else:
+                logger.warning(
+                    f"No version found in subdir of file {legacy_study_download.id}, using 0"
+                )
+
+            group_type_slug = "unknown"
+            if legacy_study_download.group_type:
+                group_type_slug = slugify(
+                    legacy_study_download.group_type.group_type.lower()
+                )
+
+            download_group = (
+                f"study_summary.v{version}.{group_type_slug.replace('-', '_')}"
+            )
+
+            study.add_download(
+                DownloadFile(
+                    path=str(path),
+                    alias=legacy_study_download.alias,
+                    long_description=legacy_study_download.description.description,
+                    short_description=legacy_study_download.description.description_label,
+                    download_type=LEGACY_DOWNLOAD_TYPE_MAP.get(
+                        legacy_study_download.group_id, DownloadType.OTHER
+                    ),
+                    download_group=download_group,
+                    file_type=LEGACY_FILE_FORMATS_MAP.get(
+                        legacy_study_download.format_id, DownloadFileType.OTHER
+                    ),
+                )
+            )
+
 
 @flow(
     name="Import all legacy analyses",
     flow_run_name="Import legacy analyses from all known studies",
 )
-def import_all_legacy_analyses(fetch_functions: bool = False):
+def import_all_legacy_analyses(fetch_functions: bool = False, mgys_after: int = 0):
     """
     Imports all legacy analyses from all known studies.
 
@@ -171,16 +236,34 @@ def import_all_legacy_analyses(fetch_functions: bool = False):
     :param fetch_functions: Boolean indicating whether to fetch associated
         functions during the import process.
     :type fetch_functions: bool
+    :param mgys_after: Only import study ids greater than this number.
+    :type mgys_after: int
     :return: None
     """
     logger = get_run_logger()
     with legacy_emg_db_session() as session:
-        study_ids_stmt = select(LegacyStudy.id)
+        study_ids_stmt = (
+            select(LegacyStudy.id)
+            .where(LegacyStudy.id > mgys_after)
+            .order_by(LegacyStudy.id.asc())
+        )
         study_ids = session.scalars(study_ids_stmt).all()
 
     logger.info(f"Found {len(study_ids)} legacy studies to import")
+    failed_study_accessions = []
 
     for study_id in study_ids:
         mgys = f"MGYS{study_id:08d}"
         logger.info(f"Importing study {mgys}")
-        import_legacy_analyses(mgys=mgys, fetch_functions=fetch_functions)
+        try:
+            import_legacy_analyses(mgys=mgys, fetch_functions=fetch_functions)
+        except Exception as e:
+            logger.error(f"Failed to import study {mgys}: {e}")
+            failed_study_accessions.append(mgys)
+
+    if failed_study_accessions:
+        create_table_artifact(
+            key="failed-legacy-study-imports",
+            table=[{"accession": accession} for accession in failed_study_accessions],
+            description=f"{len(failed_study_accessions)} studies failed to be imported from legacy EMG DB.",
+        )
