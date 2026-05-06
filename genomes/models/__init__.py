f7cc2069180f42b2bb59a9440d62cd71c70247bf
Martin Beracochea <mbc@ebi.ac.uk>
2026-05-06 21:12:28 +0100

    Sort the genomes model __init__.py - otherwise it fails
    The order of the imports is important for the Genomes as there
    is a dependency between the models

--- genomes/models/__init__.py
+++ genomes/models/__init__.py
@@ -1,7 +1,8 @@
-from .additional_contained_genomes import AdditionalContainedGenomes
 from .genome import Genome
 from .genome_assembly_link import GenomeAssemblyLink
 from .genome_catalogue import GenomeCatalogue
+
+from .additional_contained_genomes import AdditionalContainedGenomes  # isort: skip
 
 __all__ = [
     "Genome",
