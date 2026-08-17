4c1c0ffd6f46c51c92c63a927c1cd83698691c55
MGS-sails <mahfouzshehu@gmail.com>
2026-08-17 17:26:28 +0100

    Created merge migration

--- genomes/models/__init__.py
+++ genomes/models/__init__.py
@@ -2,8 +2,8 @@ from .catalogue_genome import CatalogueGenome
 from .genome import Genome
 from .genome_assembly_link import GenomeAssemblyLink
 from .genome_catalogue import GenomeCatalogue
-from .genome_search_index import GenomeSearchIndex
 from .genome_catalogue_series import GenomeCatalogueSeries
+from .genome_search_index import GenomeSearchIndex
 
 from .additional_contained_genomes import AdditionalContainedGenomes  # isort: skip
 
